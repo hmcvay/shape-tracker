243d9124049b4461d6671e5c8079a77e33245a80
hmcvay <hayleymcvay8@gmail.com>
2022-02-01 07:44:29 -0800

    Pass fourth test.

--- src/triangle.js
+++ src/triangle.js
@@ -12,6 +12,8 @@ Triangle.prototype.checkType = function() {
     return "not a triangle";
   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
     return "scalene triangle";
+  } else if ((this.side1 === this.side2) || ((this.side1 === this.side3)) || ((this.side2 === this.side3))) {
+    return "isosceles triangle";
   }
 };
 
